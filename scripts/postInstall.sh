1dfac534339466d8d8fbb1e9555b28f6327e8e81
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-05-15 11:38:35 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,7 +3,7 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 180s;
+sleep 240s;
 
 docker-compose exec -T taiga-back ./manage.py shell -c "from taiga.users.models import User; User.objects.create_superuser('admin', '"${ADMIN_EMAIL}"', '"${ADMIN_PASSWORD}"')"
 
