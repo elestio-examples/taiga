bc06762851e2778359ce144251afec0b450e05cc
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-04-04 08:49:16 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,6 +5,13 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for software to be ready ..."
 sleep 240s;
 
-docker-compose exec -T taiga-back ./manage.py shell -c "from taiga.users.models import User; User.objects.create_superuser('admin', '"${ADMIN_EMAIL}"', '"${ADMIN_PASSWORD}"')"
 
-docker-compose exec -T taiga-back sh -c "python manage.py collectstatic"
+
+
+if [ -e "./initialized" ]; then
+    echo "Already initialized, skipping..."
+else
+    docker-compose exec -T taiga-back ./manage.py shell -c "from taiga.users.models import User; User.objects.create_superuser('admin', '"${ADMIN_EMAIL}"', '"${ADMIN_PASSWORD}"')"
+    docker-compose exec -T taiga-back sh -c "python manage.py collectstatic"    
+    touch "./initialized"
+fi
